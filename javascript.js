b429342ac373e23867406ce504ed8cfeb6a20fa7
SukhOP <43757388+sukhop@users.noreply.github.com>
2021-11-20 00:02:35 +0530

    learned string method

--- javascript.js
+++ javascript.js
@@ -1,8 +1,52 @@
-
 // JavaScript Events
 // function myfun() {
 //   document.getElementById("demo").innerHTML = "Hello";
 // }
-const demo = document.getElementById("demo");
-let x = demo.innerHTML;
-console.log(x.length);
+
+// const demo = document.getElementById("demo");
+// let x = demo.innerHTML;
+// console.log(x.length);
+
+// -------------------------------------------------------------
+
+//JS String length
+// let text = "heheeeeeeeeeeeeeeeeeeeeeeeeeee";
+// console.log(text.length);
+
+    // let text = "hehe it's me SukhOPGod.";
+// JS Slice Method
+// console.log(text.slice(5, 12));
+    // console.log(text.slice(-10));
+
+//JS Substring Method
+    // console.log(text.substring(5));
+
+// JS Substr Method
+    // console.log(text.substr(13, 9));
+
+// JS Replace method
+    //(Replace does not changes the string it creates a new string.)
+    // console.log(text.replace("hehe", "Konnichiwa"));
+
+    //(Replace method is case sensitive. So, writing HEHE insted of hehe will not work.)
+    // console.log(text.replace("Hehe", "error")); --->(dosen't changes anything.)
+
+    //(But to make replace insensitive we can use /i(regular expression))
+    //(Regular Expressions are written without quotes.)
+    // console.log(text.replace(/HEHE/i, "no error"));
+
+    //(To replace all the matching word we use /g)
+    // console.log(text.replace(/hehe/g, "replaced all"));
+
+    // let txt = "Hello World"
+// JS UpperCase Method
+    //(To convert a string to upper case we use toUpperCase() function.)
+    // console.log(txt.toUpperCase());
+
+    //(To convert a string to lower case we use toLowerCase() function.)
+    // console.log(txt.toLowerCase());
+
+    // let word = "     Hello World     ";
+// JS Trim
+    //(trim() function is used to remove the whitespace from the string.)
+    // console.log(word.trim());
